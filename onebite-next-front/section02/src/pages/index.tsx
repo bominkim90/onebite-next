dca06d4c869bc4bcc8db47c72cc2957fb73c04b0
bominkim90 <bomin97@naver.com>
2025-10-01 11:31:54 +0900

    [스타일링] 파일명.module.css 파일을 import 해서 스타일링
    ## 학습 내용
    - Next.js 에서는 App 컴포넌트 이외의 컴포넌트에서 css파일을 그대로 import 하는걸 막고있음.
    - 그러나 스타일링을 각각의 컴포넌트에 대응하는 css파일로 관리하고 싶을 때는, CSS Module 방식을 쓰면 됨.
    
    ## 참고 사항
    - 실무에서는 보통 tailwind를 많이 사용한다 함

--- onebite-next-front/section02/src/pages/index.tsx
+++ onebite-next-front/section02/src/pages/index.tsx
@@ -1,3 +1,17 @@
+// App.tsx 컴포넌트가 아니라면, 이렇게 css파일을 그대로 불러오는걸 Next.js에서는 막고 있다.
+// import "./index.css";
+// 대채 방안 => CSS Module
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  // className들을 객체 형태로 담아줌
+  console.log(style);
+
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+
+      <h2 className={style.h2}>부제목</h2>
+    </>
+  );
 }
